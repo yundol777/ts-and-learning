0411cbf8d3f223882e4edfd8e04f59f6e5fd00a8
yundoll <mar0722@naver.com>
2026-03-25 18:11:59 +0900

    문제 8번 통과

--- exercises/exercise-08/index.ts
+++ exercises/exercise-08/index.ts
@@ -18,74 +18,80 @@ Exercise:
 */
 
 interface User {
-    type: 'user';
-    name: string;
-    age: number;
-    occupation: string;
+  type: "user";
+  name: string;
+  age: number;
+  occupation: string;
 }
 
 interface Admin {
-    type: 'admin';
-    name: string;
-    age: number;
-    role: string;
+  type: "admin";
+  name: string;
+  age: number;
+  role: string;
 }
 
-type PowerUser = unknown;
+type PowerUser = Omit<User, "type"> &
+  Omit<Admin, "type"> & { type: "powerUser" };
 
 export type Person = User | Admin | PowerUser;
 
 export const persons: Person[] = [
-    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
-    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
-    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
-    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
-    {
-        type: 'powerUser',
-        name: 'Nikki Stone',
-        age: 45,
-        role: 'Moderator',
-        occupation: 'Cat groomer'
-    }
+  {
+    type: "user",
+    name: "Max Mustermann",
+    age: 25,
+    occupation: "Chimney sweep",
+  },
+  { type: "admin", name: "Jane Doe", age: 32, role: "Administrator" },
+  { type: "user", name: "Kate Müller", age: 23, occupation: "Astronaut" },
+  { type: "admin", name: "Bruce Willis", age: 64, role: "World saver" },
+  {
+    type: "powerUser",
+    name: "Nikki Stone",
+    age: 45,
+    role: "Moderator",
+    occupation: "Cat groomer",
+  },
 ];
 
 function isAdmin(person: Person): person is Admin {
-    return person.type === 'admin';
+  return person.type === "admin";
 }
 
 function isUser(person: Person): person is User {
-    return person.type === 'user';
+  return person.type === "user";
 }
 
 function isPowerUser(person: Person): person is PowerUser {
-    return person.type === 'powerUser';
+  return person.type === "powerUser";
 }
 
 export function logPerson(person: Person) {
-    let additionalInformation: string = '';
-    if (isAdmin(person)) {
-        additionalInformation = person.role;
-    }
-    if (isUser(person)) {
-        additionalInformation = person.occupation;
-    }
-    if (isPowerUser(person)) {
-        additionalInformation = `${person.role}, ${person.occupation}`;
-    }
-    console.log(`${person.name}, ${person.age}, ${additionalInformation}`);
+  let additionalInformation: string = "";
+  if (isAdmin(person)) {
+    additionalInformation = person.role;
+  }
+  if (isUser(person)) {
+    additionalInformation = person.occupation;
+  }
+  if (isPowerUser(person)) {
+    additionalInformation = `${person.role}, ${person.occupation}`;
+  }
+  console.log(`${person.name}, ${person.age}, ${additionalInformation}`);
 }
 
-console.log('Admins:');
+console.log("Admins:");
 persons.filter(isAdmin).forEach(logPerson);
 
 console.log();
 
-console.log('Users:');
+console.log("Users:");
 persons.filter(isUser).forEach(logPerson);
 
 console.log();
 
-console.log('Power users:');
+console.log("Power users:");
 persons.filter(isPowerUser).forEach(logPerson);
 
 // In case you are stuck:
