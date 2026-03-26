bcaf06dba910eb5f0fb40b1043a86af537f84ef1
yundoll <mar0722@naver.com>
2026-03-26 16:07:37 +0900

    문제 11번 통과

--- exercises/exercise-11/declarations/str-utils/index.d.ts
+++ exercises/exercise-11/declarations/str-utils/index.d.ts
@@ -1,4 +1,7 @@
-declare module 'str-utils' {
-    // export const ...
-    // export function ...
+declare module "str-utils" {
+  export function strReverse(value: string): string;
+  export function strToLower(value: string): string;
+  export function strToUpper(value: string): string;
+  export function strRandomize(value: string): string;
+  export function strInvertCase(value: string): string;
 }
